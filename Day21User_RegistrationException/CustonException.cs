2306665bb8f0ce5a1fe245d3ad07c5d957452102
Tejassonar <sonartejas908@gmail.com>
2021-10-13 20:13:50 +0530

    Added custom exceptions for testing

---
+++ Day21User_RegistrationException/CustonException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day21User_RegistrationException
+{
+    public class MyException : Exception
+    {
+        public MyException(string Message) : base(Message)
+        {
+
+        }
+        public MyException()
+        {
+
+        }
+        
+    }
+}
